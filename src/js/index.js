8a3c4c407d9e760c9c95f0911f8abb2a33c4683c
Gabriel Tobias <102703941+SargenTobieus@users.noreply.github.com>
2023-02-27 00:37:58 -0300

    Add files via upload

---
+++ src/js/index.js
@@ -0,0 +1,82 @@
+let number1 = document.getElementById('1');
+let number2 = document.getElementById('2');
+let number3 = document.getElementById('3');
+let number4 = document.getElementById('4');
+let number5 = document.getElementById('5');
+let number6 = document.getElementById('6');
+let number7 = document.getElementById('7');
+let number8 = document.getElementById('8');
+let number9 = document.getElementById('9');
+let number10 = document.getElementById('10');
+
+var result = document.getElementById('result');
+
+const diabinho = '<img src="./src/img/diabinho.png" alt="">'
+const anjo = '<img src="./src/img/anjo.png" alt="">'
+
+
+number1.addEventListener('click', function (event) {
+    event.preventDefault
+    alert('Você não está triste');
+    result.innerHTML = anjo;
+});
+
+number2.addEventListener('click', function (event) {
+    event.preventDefault
+    alert('Você não está triste');
+    result.innerHTML = anjo;
+});
+
+number3.addEventListener('click', function (event) {
+    event.preventDefault
+    alert('Você não está triste');
+    result.innerHTML = anjo;
+});
+
+number3.addEventListener('click', function (event) {
+    event.preventDefault
+    alert('Você não está triste');
+    result.innerHTML = anjo;
+});
+
+number4.addEventListener('click', function (event) {
+    event.preventDefault
+    alert('Você não está triste');
+    result.innerHTML = anjo;
+});
+
+number5.addEventListener('click', function (event) {
+    event.preventDefault
+    alert('Você está triste então precisa se animar');
+    result.innerHTML = diabinho;
+});
+
+number6.addEventListener('click', function (event) {
+    event.preventDefault
+    alert('Você está triste então precisa se animar');
+    result.innerHTML = diabinho;
+});
+
+number7.addEventListener('click', function (event) {
+    event.preventDefault
+    alert('Você está triste então precisa se animar');
+    result.innerHTML = diabinho;
+});
+
+number8.addEventListener('click', function (event) {
+    event.preventDefault
+    alert('Você está triste então precisa se animar');
+    result.innerHTML = diabinho;
+});
+
+number9.addEventListener('click', function (event) {
+    event.preventDefault
+    alert('Você está triste então precisa se animar');
+    result.innerHTML = diabinho;
+});
+
+number10.addEventListener('click', function (event) {
+    event.preventDefault
+    alert('Você está triste então precisa se animar');
+    result.innerHTML = diabinho;
+});
